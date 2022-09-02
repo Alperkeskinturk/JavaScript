26a0609a8897674de63cf6a3f8fc7410cd73a699
Alper Keskinturk <alper@MSI-A01332.mps.k12>
2022-09-02 12:58:06 -0700

    Update

--- js-basics/index.js
+++ js-basics/index.js
@@ -1,2 +1,11 @@
-// This is my first Javascript code!
-console.log('Hello World')
+let person = {
+    name: 'Alper',
+    age: 15
+};
+
+person.name = 'Alper';
+
+let selection = 'name';
+person[selection] = 'Alper';
+
+console.log(person.name);
