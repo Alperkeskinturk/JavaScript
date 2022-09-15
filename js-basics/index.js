bf81223096f13c0ab0d331b1c4fea8e0033d954a
Alper Keskinturk <alper@MSI-A01332.mps.k12>
2022-09-15 13:00:44 -0700

    update

--- js-basics/index.js
+++ js-basics/index.js
@@ -1,4 +1,4 @@
-
+/*
 function greet(name, lastName){
     console.log(name + ' ' + lastName + ' is the best programmer.')
 };
@@ -25,3 +25,36 @@ function addUp(begin, finish){
     return total;
 }
 console.log(addUp(2,9))
+
+let counter = 1;
+while (counter <= 100){
+    console.log(counter);
+    counter++
+}
+
+let password="a"
+let userInput=null
+
+while (userInput !== password){
+    userInput=prompt("Please enter the right password")
+}
+alert("Access granted")
+
+let myName=prompt("Please say your name")
+alert("Welcome "+myName+ " to your website")
+*/
+
+let hour = 19;
+
+if (hour>= 6 && hour< 12) {
+    console.log("Good Morning");
+}
+else if (hour >= 12 && hour < 1) {
+    console.log('Good Afternoon')
+}
+else if(hour >= 17 && hour < 21) {
+    console.log('Good Evening');
+}
+else {
+    console.log('Good Night')
+}
